9545ad19e7edf4ac65a2ddbe1502dbb0f8ed080c
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-28 13:12:15 +0330

    Added CSS changes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-6 main-part">
-            <h1>Get insights that help your business grow.</h1>
+            <h1>
+              Get <spn className="insight">insights</spn> that help your
+              business grow.
+            </h1>
             <p>
               {" "}
               Discover the benefits of data analytics and make better decisions
               regarding revenue, customer experience, and overall efficiency.{" "}
             </p>
-            <div className="row">
+            <div className="row furtherInfo">
               <div className="col-4">
-                10k+ <div>ompanies</div>
+                <span className="number">10k+</span>{" "}
+                <div className="info">COMPANIES</div>
               </div>
               <div className="col-4">
-                314 <div>templates</div>
+                <span className="number">314</span>{" "}
+                <div className="info">TEMPLATES</div>
               </div>
               <div className="col-4">
-                2m+ <div>queries</div>
+                <span className="number">12M+</span>
+                <div className="info">QUERIES</div>
               </div>
             </div>
           </div>
